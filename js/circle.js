0025fb6e7dfcc7c19ac2f9f6b05b350906344c61
Billie <billie.dorries28@gmail.com>
2022-12-05 15:26:58 -0600

    Completed Math object exercise

---
+++ js/circle.js
@@ -0,0 +1,44 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            return Math.PI*(Math.pow(this.radius,2)) // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            if(doRounding){
+                let rounded = Math.round((Math.PI*(Math.pow(this.radius,2))*100)/100);
+                console.log(`Area of a circle with radius: ${this.radius}, is: ${rounded}`);
+            } else
+
+            console.log(`Area of a circle with radius: ${this.radius}, is: ${Math.PI*(Math.pow(this.radius,2))}`);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
